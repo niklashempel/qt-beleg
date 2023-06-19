9144581c8f4cc6d16f4d37cc9bfaf593f6c1683d
Niklas Hempel <66086679+niklashempel@users.noreply.github.com>
2023-06-19 21:07:40 +0200

    Change code behind

--- src/views/mainwindow.h
+++ src/views/mainwindow.h
@@ -2,45 +2,46 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
 #include "../lib/datastore.hpp"
 #include "../models/medium.hpp"
 #include "../models/person.hpp"
 #include <QDialogButtonBox>
+#include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui
-{
-    class MainWindow;
+namespace Ui {
+class MainWindow;
 }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 
 {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
-    void LoadMedia();
-    void LoadPeople();
-    void AddMedium();
-    void AddPerson();
-    void EditPerson(Person *person);
-    void EditMedium(Medium *medium);
-    void InitializeUi();
-    void SetAddMediumVisible(bool);
-    void SetMediaListVisible(bool);
-    void SetAddPersonVisible(bool);
-    void SetPersonListVisible(bool);
-    Datastore<Medium> mediumStore;
-    Datastore<Person> personStore;
-    QDialogButtonBox *mediaDialogButtonBox;
-    QDialogButtonBox *peopleDialogButtonBox;
+  Ui::MainWindow *ui;
+  void LoadMedia();
+  void LoadPeople();
+  void AddMedium();
+  void AddPerson();
+  void EditPerson(Person *person);
+  void EditMedium(Medium *medium);
+  void InitializeUi();
+  void SetAddMediumVisible(bool);
+  void SetMediaListVisible(bool);
+  void SetAddPersonVisible(bool);
+  void SetPersonListVisible(bool);
+  Datastore<Medium> mediumStore;
+  Datastore<Person> personStore;
+  QDialogButtonBox *mediaDialogButtonBox;
+  QDialogButtonBox *peopleDialogButtonBox;
+  Medium *selectedMedium;
+  Person *selectedPerson;
 };
 
 #endif // MAINWINDOW_H
