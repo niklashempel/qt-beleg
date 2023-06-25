3dc44652b4f6a29cca19d31c70617af898151764
Niklas Hempel <66086679+niklashempel@users.noreply.github.com>
2023-06-25 11:18:14 +0200

    Set application icon and title

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "qstyle.h"
 #include "views/mainwindow.h"
 #include <QApplication>
 #include <QString>
@@ -6,6 +7,7 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
+  a.setWindowIcon(QApplication::style()->standardIcon(QStyle::SP_DesktopIcon));
   MainWindow w;
 
   w.show();
